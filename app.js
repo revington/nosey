d25b48116decd8873b941ac3a0548b093038640d
Pedro Narciso García Revington <p.revington@gmail.com>
2012-08-25 02:23:18 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    simplesmtp = require('simplesmtp'),
+    MailParser = require('mailparser').MailParser;
+var app = express();
+app.configure(function () {
+    app.set('smtp port', process.env.SMTP_PORT || 4040);
+    app.set('port', process.env.PORT || 4000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+var smtp = simplesmtp.createServer({
+    debug: true
+}),
+    db = [];
+app.get('/', function (req, res) {
+    res.format({
+        json: function () {
+            res.json(db);
+        },
+        html: function () {
+            res.render('index', {
+                smtpPort: app.get('smtp port')
+            });
+        }
+    });
+});
+smtp.listen(app.get('smtp port'), function (err) {
+    smtp.on("startData", function (envelope) {
+        envelope.buffer = "";
+    });
+    smtp.on("data", function (envelope, chunk) {
+        envelope.buffer += chunk;
+    });
+    smtp.on("dataReady", function (envelope, callback) {
+        var mailParser = new MailParser();
+        mailParser.on('end', function (email) {
+            db.push(email);
+        });
+        mailParser.write(envelope.buffer);
+        mailParser.end();
+        callback(null);
+    });
+    http.createServer(app).listen(app.get('port'), function () {
+        console.log("Nosey web server listening on port " + app.get('port'));
+    });
+});
---
+++ public/javascripts/nosey.js
@@ -0,0 +1,103 @@
+var nosey = (function () {
+    function iHaveTemplate(self) {
+        self.template = function (item) {
+            return item ? item.template() : self.type() + '-' + self.mode();
+        };
+    }
+
+    function capitalize(input) {
+        if (!input) {}
+        return input[0].toUpperCase() + input.slice(1);
+    }
+
+    function initHelper(self, data, simple, mapping) {
+        var prop, sprop;
+        if (simple) {
+            for (prop in simple) {
+                if (simple.hasOwnProperty(prop)) {
+                    data[prop] = data[prop] || simple[prop];
+                }
+            }
+        }
+        mapping = mapping || {};
+        if (simple) {
+            for (prop in simple) {
+                if (simple.hasOwnProperty(prop) && $.isArray(simple[prop])) {
+                    sprop = inflector.singularize(prop);
+                    if (!sprop) {
+                        sprop = prop;
+                    }
+                    sprop = capitalize(sprop);
+                    if (!nosey[sprop]) {
+                        continue;
+                    }
+                    mapping[prop] = (function (constructor) {
+                        return {
+                            create: function (options) {
+                                return new constructor(options.data, self);
+                            }
+                        };
+                    })(nosey[sprop]);
+                }
+            }
+        }
+        ko.mapping.fromJS(data, mapping, self);
+        self.mode = ko.observable('abstract');
+        iHaveTemplate(self);
+    }
+
+    function Address(data) {
+        var self = this;
+        data = data || {};
+        initHelper(self, data, {
+            type: 'address',
+            address: undefined,
+            name: undefined
+        }, null);
+    }
+
+    function Email(data) {
+        data = data || {};
+        var self = this;
+        initHelper(self, data, {
+            type: 'email',
+            subject: undefined,
+            html: undefined,
+            text: undefined,
+            date: undefined,
+            priority: undefined,
+            from: [],
+            to: []
+        }, {
+            from: {
+                create: function (options) {
+                    return new Address(options.data);
+                }
+            },
+            to: {
+                create: function (options) {
+                    return new Address(options.data);
+                }
+            }
+        });
+				self.showDetails = function(){
+					self.mode('details');
+				};
+				self.showAbstract = function(){
+					self.mode('abstract');
+				};
+    }
+
+    function EmailCollection(data) {
+        data = data || {};
+        var self = this;
+        initHelper(self, data, {
+            type: 'email-collection',
+            emails: []
+        }, null);
+    }
+    return {
+        Email: Email,
+        EmailCollection: EmailCollection
+    };
+})();
